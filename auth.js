270cc4055a0be3f0e62674fa7a5179d7996973a1
Konstantin Matiukhin <matuhin_228007@mail.ru>
2023-03-28 20:50:12 +0300

    1

--- auth.js
+++ auth.js
@@ -2,20 +2,34 @@ const login = document.querySelector('.login1')
 const password = document.querySelector('.parol1')
 const error = document.querySelector('.authError')
 
-let data = [['alex', 'qwerty'], ['slava', '1231234'], ['123', '123']]
-
 const button = document.querySelector('.voity')
-button.addEventListener('click', () => {
-    let flag = 0
-    data.forEach(element => {
-        if (login.value == element[0] && password.value == element[1]){
-            flag++
+
+
+
+async function auth(){
+    let response = await fetch('https://api.baserow.io/api/database/rows/table/152130/?user_field_names=true', {
+        headers:{
+            Authorization: 'Token v1hcfl8ldK6GuvpNzlPXcQBEO91TeGjn'
         }
-    });
-    if (flag == 1){
-        button.href = "glavnaya.html"
-    }
-    else{
-        error.style.visibility = 'visible'
-    }
-})
+    })
+    let contentJson = await response.json()
+    let count = contentJson.count
+
+    button.addEventListener('click', () => {
+        let flag = 0
+        for (let i = 0; i < count; i++){
+            if (login.value == contentJson.results[i].Login && password.value == contentJson.results[i].Password){
+                flag++
+                break
+            }
+        }
+        if (flag == 1){
+            button.href = "glavnaya.html"
+        }
+        else{
+            error.style.visibility = 'visible'
+        }
+    })
+}
+
+auth()
